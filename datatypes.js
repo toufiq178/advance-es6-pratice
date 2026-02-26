bda47a004e5b961be510a1b53bfa15bfdb61ac25
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-26 23:35:16 +0600

    first commit

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+
+// primitive data types
+
+const name = 'taj';
+const number = 2;
+const married = false;
+
+const x = null
+const y = [];
+const z = {};
+
+
+
+// console.log('name type', typeof name);
+// console.log('number type', typeof number);
+// console.log('married type', typeof married);
+// console.log('x type', typeof x);
+// console.log('y type', typeof y);
+// console.log('z type', typeof z);
+
+
+
+// non primitive
+
+const array = [1, 2, 3, 4, 5];
+const obj = { price : 30}
+
+console.log(typeof array);
+console.log(typeof obj);
+
---
+++ undefine.js
@@ -0,0 +1,32 @@
+
+let x;
+
+// console.log(x);
+
+
+function name(n) {
+
+
+    // console.log(n);
+
+}
+
+name()
+
+function y() {
+
+
+}
+// console.log(y());
+
+
+const obj = { name: 'taj', age: 20 }
+
+delete obj.name
+// console.log(obj.name);
+
+const array = [1, 2, 3, 4, 5]
+
+// delete array[0]
+
+console.log(array[0]);
