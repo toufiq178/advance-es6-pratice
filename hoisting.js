befafdc48a4ad30e6bd044a61b0276b0d1c2b710
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-01 01:59:05 +0600

    problem solving

--- hoisting.js
+++ hoisting.js
@@ -1,18 +1,109 @@
 // Task 1: Hoisting
 // console.log(a);
-var a = 20;
+// var a = 20;
 
-// console.log(b);
-let b = 30;
+// // console.log(b);
+// let b = 30;
 
 
-// Task 2: Scope
-let x = 10;
+// // Task 2: Scope
+// let x = 10;
 
-function test() {
-  let x = 20;
-  console.log(x);
+// function test() {
+//   let x = 20;
+//   console.log(x);
+
+// }
+
+// test();
+// console.log(x);
+
+
+// let vs const
+
+// const price2 = 100;
+// // price2 = 200;
+// console.log(price2);
+
+
+
+// let price = 100;
+// price = 200;
+// console.log(price);
+
+
+
+
+// // ✅ Task 4: Default Parameter
+
+// function welcome(name = 'Guest' ) {
+    
+//     console.log('welcome', name);
+    
+// }
+
+// welcome('taj')
+// welcome()
+
+
+// ✅ Task 1: Rest (Function)
+// Eta function sumAll banao:
+// joto gula number diba
+// shob gula jog kore return korbe
+// rest operator use korte hobe
+
+
+function symAll(...Parameter) {
+    
+    // console.log(Parameter);
+    let total = 0
+    for(elem of Parameter){
+
+        total += elem
+    }
+    // console.log(total);
+    
 }
 
-test();
-console.log(x);
+symAll(5,10,15)
+
+
+
+// ✅ Task 2: Rest (Destructuring)
+// Nicher array theke:
+// first value alada variable e rakho
+// baki gula ekta array te rakho (rest use kore)
+
+
+const [first , ...others] = [10, 20, 30, 40, 50];
+
+// const first = numbers[0]
+// const others = [ n,  ...numbers]
+// console.log(first);
+
+// console.log(first , '||', others);
+
+
+// ✅ Task 3: Array Merge
+// Spread use kore ekta students array banao jekhane sob thakbe.
+
+const boys = ["Rahim", "Karim"];
+const girls = ["Ayesha", "Mim"];
+
+// const sts = [boys , girls]
+
+const students = [...boys , ...girls]
+// console.log(students);
+
+
+// Task 4: Array Copy Problem (Important)
+// 👉 Problem ta explain koro.
+// 👉 Spread use kore eta fix koro jate arr1 change na hoy
+
+const arr1 = [1,2,3];
+const arr2 = [...arr1];
+
+arr2.push(4);
+
+console.log(arr1);
+console.log(arr2);
